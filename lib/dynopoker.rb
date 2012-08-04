b05c4efd37c77698818e575baee05d496f244e0a
Kuba <kubenstein@gmail.com>
2012-08-04 23:03:49 +0200

    loop braking condition

--- lib/dynopoker.rb
+++ lib/dynopoker.rb
@@ -8,18 +8,22 @@ module Dynopoker
 
 
   def self.start(address=nil)
-    if ENV['stopDynoPoking'] || self.disabled
+    unless should_poke?
       puts "Dynopoker: poking disabled"
       return
     end
 
     self.address = address unless address.nil?
     raise "Dynopoker: set address to poke first" if self.address.nil?
-    Thread.new { while true do sleep(3600); poke;end }
+    Thread.new { until should_poke? do sleep(3600); poke; end }
   end
 
   def self.poke
     puts "Dynopoker: poking #{self.address}"
     open(self.address).content_type
+  end
+
+  def should_poke?
+    !ENV['stopDynoPoking'] && !self.disabled
   end
 end
