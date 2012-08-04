b2119e321a0ecd319589f53c5da4e750c41cfdad
Kuba <kubenstein@gmail.com>
2012-08-04 16:45:33 +0200

    poking logic

--- dynopoker.gemspec
+++ dynopoker.gemspec
@@ -8,8 +8,8 @@ Gem::Specification.new do |s|
   s.authors     = ["Kubenstein"]
   s.email       = ["kubenstein@gmail.com"]
   s.homepage    = ""
-  s.summary     = %q{TODO: Write a gem summary}
-  s.description = %q{TODO: Write a gem description}
+  s.summary     = %q{Write a gem summary}
+  s.description = %q{Write a gem description}
 
   s.rubyforge_project = "dynopoker"
 
--- lib/dynopoker.rb
+++ lib/dynopoker.rb
@@ -1,5 +1,25 @@
 require "dynopoker/version"
+require 'open-uri'
 
 module Dynopoker
-  # Your code goes here...
+  class << self
+    attr_accessor :address
+  end
+
+
+  def self.start(address=nil)
+    self.address = address unless address.nil?
+    raise "Dynopoker: set address to poke first" if self.address.nil?
+
+    if ENV['stopDynoPoking']
+      puts "Dynopoker: poking disabled by setting ENV['stopDynoPoking']"
+    else
+      Thread.new { while true do sleep(3600); poke; end }
+    end
+  end
+
+  def self.poke
+    puts "Dynopoker: poking #{self.address}"
+    open(self.address).content_type
+  end
 end
