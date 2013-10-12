0d5a6bce92b9aadc2d5d6d47bde27aabb3c343e4
Kuba <kubenstein@gmail.com>
2013-10-12 22:49:11 +0200

    gem simplification

--- lib/dynopoker.rb
+++ lib/dynopoker.rb
@@ -1,43 +1,52 @@
-require "dynopoker/version"
-require 'dynopoker/configuration'
+require 'dynopoker/version'
 require 'open-uri'
 
 module Dynopoker
-  class << self
-    attr_accessor :configuration, :already_started
-  end
 
   def self.configure
-    self.configuration ||= Configuration.new
-    yield configuration
+    Poker.new.tap { |p| yield p }.start!
+  end
 
-    if configuration.should_poke? && !already_started
-      self.already_started = true
-      self.start
+  class Poker
+    attr_accessor :enable, :address, :poke_frequency, :logger
+
+    def start!
+      merge_default_options!
+      start_poking_thread! if should_poke?
     end
-  end
 
-  def self.start
-    configuration.logger.info 'Dynopoker: starting thread'
-    Thread.new { poke } or configuration.logger.error 'Dynopoker: error while starting thread'
-  end
 
-  def self.poke
-    while(true)
-      begin
-        configuration.logger.info "Dynopoker: poking #{self.configuration.address}"
-        open(configuration.address).content_type
-      rescue Exception => exception
-        configuration.logger.error "Dynopoker: poking error #{exception.class.name}: #{exception.message}"
+    private
+
+    def start_poking_thread!
+      logger.info 'Dynopoker: starting thread'
+      Thread.new { poking } or logger.error 'Dynopoker: error while starting thread'
+    end
+
+    def poking
+      while true
+        poke!
+        sleep(poke_frequency)
       end
-
-      sleep(configuration.poke_frequency)
     end
+
+    def poke!
+      logger.info "Dynopoker: poking #{address}"
+      open(address).status
+    rescue Exception => exception
+      configuration.logger.error "Dynopoker: poking error #{exception.class.name}: #{exception.message}"
+    end
+
+    def merge_default_options!
+      self.poke_frequency ||= 1800
+      self.logger ||= Logger.new($stdout)
+      self.enable = enable.nil? ? true : enable
+      raise('Dynopoker: no address provided!') if !address && enable
+    end
+
+    def should_poke?
+      address.present? && enable && poke_frequency.present?
+    end
+
   end
-
 end
-
-if defined? Rails
-  require 'dynopoker/rails'
-end
-
--- lib/dynopoker/railtie.rb
+++
@@ -1,9 +0,0 @@
-module Dynopoker
-  # Connects to integration points for Rails 3 applications
-  class Railtie < ::Rails::Railtie
-    initializer :initialize_dynopoker_rails, :after => :before_initialize do
-      Dynopoker::Rails.initialize
-    end
-  end
-end
-
